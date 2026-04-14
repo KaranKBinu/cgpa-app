cfe04dcf7e4d963387add86e2d57eee67beb452e
Trusthire <trusthiresystem@gmail.com>
2026-04-14 06:47:35 +0530

    feat: implement authentication middleware and update Next.js configuration for path resolution

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.resolve(__dirname),
+    resolveAlias: {
+      tailwindcss: path.resolve(__dirname, "cgpa-app/node_modules/tailwindcss"),
+    },
+  },
 };
 
 export default nextConfig;
